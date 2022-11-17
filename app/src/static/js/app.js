81bfe0cd2024a905df582ef29416bf35ac292a0b
Jprl97 <116816830+Jprl97@users.noreply.github.com>
2022-11-17 14:34:25 +0000

    Update app.js

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Que que voce quer!</p>
+                <p className="text-center"> O que que voce quer saber!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
